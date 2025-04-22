912432052ec20301add7a7abc0aee863bdd3ec73
Tembanator <49640058+Tembanator@users.noreply.github.com>
2025-04-23 00:47:53 +0200

    hero section complete

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Hero from "./sections/Hero"
+import Navbar from "./sections/Navbar"
+
+function App() {
+
+  return (
+    <div className="container mx-auto max-w-7xl">
+      <Navbar />
+      <Hero />
+      <section className="h-screen"></section>
+      <section className="h-screen"></section>
+      <section className="h-screen"></section>
+      <section className="h-screen"></section>
+      <section className="h-screen"></section>
+      <section className="h-screen"></section>
+      {/* About */}
+      {/* Projects */}
+      {/* Experience */}
+      {/* Testimonial */}
+      {/* Contact  */}
+      {/* Footer */}
+    </div>
+  )
+}
+
+export default App
