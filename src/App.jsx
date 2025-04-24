3a03907adacac888bf1d7b3ba690d8fdcf89ed59
Tembanator <49640058+Tembanator@users.noreply.github.com>
2025-04-24 11:51:10 +0200

    experiences section complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./sections/About"
+import Experiences from "./sections/Experiences"
 import Hero from "./sections/Hero"
 import Navbar from "./sections/Navbar"
 import Projects from "./sections/Projects"
@@ -11,7 +12,7 @@ function App() {
       <Hero />
       <About />
       <Projects />
-      {/* Experience */}
+      <Experiences />
       {/* Testimonial */}
       {/* Contact  */}
       {/* Footer */}
