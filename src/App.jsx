009621a57a48aed8631736667757466da91faee7
Tembanator <49640058+Tembanator@users.noreply.github.com>
2025-04-24 13:02:01 +0200

    contact section complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./sections/About"
+import Contact from "./sections/Contact"
 import Experiences from "./sections/Experiences"
 import Hero from "./sections/Hero"
 import Navbar from "./sections/Navbar"
@@ -15,7 +16,7 @@ function App() {
       <Projects />
       <Experiences />
       <Testimonial />
-      {/* Contact  */}
+      <Contact />
       {/* Footer */}
     </div>
   )
