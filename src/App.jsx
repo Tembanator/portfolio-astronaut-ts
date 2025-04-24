a45ad044b4a2173447e70239405cdb0781f55b6c
Tembanator <49640058+Tembanator@users.noreply.github.com>
2025-04-24 12:03:45 +0200

    reviews section complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Experiences from "./sections/Experiences"
 import Hero from "./sections/Hero"
 import Navbar from "./sections/Navbar"
 import Projects from "./sections/Projects"
+import Testimonial from "./sections/Testimonial"
 
 function App() {
 
@@ -13,7 +14,7 @@ function App() {
       <About />
       <Projects />
       <Experiences />
-      {/* Testimonial */}
+      <Testimonial />
       {/* Contact  */}
       {/* Footer */}
     </div>
