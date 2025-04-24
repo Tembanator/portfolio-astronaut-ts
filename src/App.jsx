100c699f333b7fe06039df67bab00d14d51d562b
Tembanator <49640058+Tembanator@users.noreply.github.com>
2025-04-24 13:08:53 +0200

    footer section complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import About from "./sections/About"
 import Contact from "./sections/Contact"
 import Experiences from "./sections/Experiences"
+import Footer from "./sections/Footer"
 import Hero from "./sections/Hero"
 import Navbar from "./sections/Navbar"
 import Projects from "./sections/Projects"
@@ -17,7 +18,7 @@ function App() {
       <Experiences />
       <Testimonial />
       <Contact />
-      {/* Footer */}
+      <Footer />
     </div>
   )
 }
--- src/contants/index.js
+++ src/contants/index.js
@@ -226,17 +226,17 @@ export const myProjects = [
 export const mySocials = [
     {
         name: "WhatsApp",
-        href: "",
+        href: "#",
         icon: "/assets/socials/whatsApp.svg",
     },
     {
         name: "Linkedin",
-        href: "https://www.linkedin.com/in/ali-sanati/",
+        href: "#",
         icon: "/assets/socials/linkedIn.svg",
     },
     {
         name: "Instagram",
-        href: "https://www.instagram.com/ali.sanatidev/reels/",
+        href: "#",
         icon: "/assets/socials/instagram.svg",
     },
 ];
